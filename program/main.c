34ea0ae057e5def891fd1f9c0087cb140c237823
valediazzapata707@gmail.com <valediazzapata@gmail.com>
2026-01-11 21:27:40 -0600

    entrega 1

---
+++ program/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv)
+{
+    int base = 5;
+    int altura = 5;
+    int profundidad = 5;
+    int area = base * altura;
+    printf("Area: %i\n", area);
+    return 0;
+}
