bb645a3f7ffebdb91779614a2fcbd4a1504e7a50
valediazzapata707@gmail.com <valediazzapata@gmail.com>
2026-01-16 11:24:52 -0600

    primera entrega: area & volumen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int area(int side){    
+    int a = side * side; /*El flotante llega hasta un determinado número*/
+    return a;
+}
+
+int volume(int side){    
+    int v = side * side * side;
+    return v;
+}
+
+int main(){
+    int side = 0;
+    
+    printf("Enter the side lenght:");
+    scanf("%i", &side);
+
+    int a = area(side);
+    int v = volume(side);
+    
+    printf("Square area: %i\n", a);
+    printf("Cube volume: %i\n",v);
+    
     return 0;
 }
